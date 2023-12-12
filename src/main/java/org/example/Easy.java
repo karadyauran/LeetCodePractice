136ac23776b17d07a2018721b4d6662e8d8d7bac
karadyauran <alexander.karadyaur@gmail.com>
2023-12-12 11:01:24 +0100

    Commit searchInsert task (35)

--- src/main/java/org/example/Easy.java
+++ src/main/java/org/example/Easy.java
@@ -145,8 +145,22 @@ public class Easy {
     return 0;
   }
 
+  // 35
+  public int searchInsert(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left <= right) {
+      int middle = left + (right - left) / 2;
+      if (nums[middle] < target) left = middle + 1;
+      else right = middle - 1;
+    }
+
+    return left;
+  }
+
   // for tests
   public static void main(String[] args) {
-    strStr("mississippi", "issip");
+
   }
 }
