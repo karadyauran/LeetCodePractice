5b90fed3518d9e6da171d548cf1b1df17622abe6
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2023-12-27 11:20:25 +0100

    Commit myPow task (50)

--- src/main/java/org/example/Medium.java
+++ src/main/java/org/example/Medium.java
@@ -221,7 +221,29 @@ public class Medium {
     return sum;
   }
 
+  // 50
+  public static double myPow(double x, int n) {
+    if (n < 0) {
+      n = -n;
+      x = 1 / x;
+    }
+
+    double pow = 1;
+
+    while (n != 0) {
+      if ((n & 1) != 0) {
+        pow *= x;
+      }
+
+      x *= x;
+      n >>>= 1;
+
+    }
+
+    return pow;
+  }
+
   public static void main(String[] args) {
-    System.out.println(makeIntegerBeautiful(16, 6));
+    System.out.println(myPow(2.0, -2));
   }
 }
